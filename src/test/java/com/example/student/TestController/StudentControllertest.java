79f2d94bcbeb9f0bc2fa5e798e8b8511e1f995c4
Choudhary <2309821@cognizant.com>
2024-12-27 15:41:17 +0530

    Added Junit for DTO class and changes for testController

--- src/test/java/com/example/student/TestController/StudentControllertest.java
+++ src/test/java/com/example/student/TestController/StudentControllertest.java
@@ -4,10 +4,19 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.web.servlet.MockMvc;
 import org.junit.jupiter.api.Test;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -34,18 +43,24 @@ public class StudentControllertest {
     StudentService studentService;
 
     @Test
-    void testgetAll() throws Exception {
-        List<Student> students = Arrays.asList(new Student("1","Daksh",24,"A+","Bijnor"));
-        when(studentService.getAllStudents()).thenReturn(students);
-        mockMvc.perform(get("/allStudents"))
+void testgetAll() throws Exception {
+    List<Student> students = Arrays.asList(new Student("1","Daksh",24,"A+","Bijnor"));
+
+    Pageable pageable = PageRequest.of(0, 10);
+    Page<Student> studentPage = new PageImpl<>(students, pageable, students.size());
+
+    when(studentService.getAllStudents(0,10)).thenReturn(studentPage);
+
+    mockMvc.perform(get("/allStudents?page=0&size=10")
+            .contentType("application/json"))
             .andExpect(status().isOk())
             .andExpect(content().contentType("application/json"))
-            .andExpect(jsonPath("$[0].name").value("Daksh")) 
-            .andExpect(jsonPath("$[0].age").value(24)) 
-            .andExpect(jsonPath("$[0].grade").value("A+")) 
-            .andExpect(jsonPath("$[0].address").value("Bijnor")); 
-            
-        }
+            .andExpect(jsonPath("$.content[0].id").value("1"))
+            .andExpect(jsonPath("$.content[0].name").value("Daksh"))
+            .andExpect(jsonPath("$.content[0].age").value(24))
+            .andExpect(jsonPath("$.content[0].grade").value("A+"))
+            .andExpect(jsonPath("$.content[0].address").value("Bijnor"));
+}
 
     @Test
      void testAddStudent() throws Exception { 
@@ -70,4 +85,13 @@ public class StudentControllertest {
          .andExpect(jsonPath("$.grade").value("A+"))
          .andExpect(jsonPath("$.address").value("Bijnor"));
         }
+
+        @Test
+         void testDelete() throws Exception { 
+              doNothing().when(studentService).deleteStudent("1"); 
+              mockMvc.perform(delete("/delete/1")) 
+              .andExpect(status().isOk());
+              verify(studentService).deleteStudent("1"); }
+        
+        
     }       
---
+++ src/test/java/com/example/student/TestModel/TestStudent.java
@@ -0,0 +1,26 @@
+package com.example.student.TestModel;
+
+import org.junit.jupiter.api.Test;
+
+import com.example.student.Model.Student;
+import org.junit.jupiter.api.Assertions;
+
+public class TestStudent {
+
+    @Test
+    void testStudent() {
+        Student student = new Student();
+        student.setId("1");
+        student.setName("Daksh");
+        student.setAge(24);
+        student.setGrade("A+");
+        student.setAddress("Choudhary Puram");
+
+        Assertions.assertEquals("1",student.getId());
+        Assertions.assertEquals("Daksh", student.getName());
+        Assertions.assertEquals(24, student.getAge());  
+        Assertions.assertEquals("A+", student.getGrade());
+        Assertions.assertEquals("Choudhary Puram", student.getAddress());
+
+    }
+}
